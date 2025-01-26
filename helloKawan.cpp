6933c6a7b467b72c53170f672e8a19f6f2e89792
CodeMstr14Trihaji <codemastermind14@gmail.com>
2025-01-27 05:07:30 +0700

    trbaru ganjil genap

---
+++ helloKawan.cpp
@@ -0,0 +1,18 @@
+// kode program C++ menentukan inputan ganjil attau genap
+#include <iostream>
+using namespace std;
+
+int main() {
+    int angka;
+    cout << "Masukkan angka: ";
+    cin >> angka;
+
+    // mengecek apakah angka tersebut genap atau ganjil
+    if (angka % 2 == 0) {
+        cout << angka << " adalah angka genap";
+    } else {
+        cout << angka << " adalah angka ganjil";
+    }
+
+    return 0;
+}
