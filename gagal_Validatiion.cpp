45f69ed312aa110b2a4ee75fa51bb72b3d432b7d
CodeMstr14Trihaji <codemastermind14@gmail.com>
2025-01-27 03:43:28 +0700

    terbaru nih hehe

---
+++ gagal_Validatiion.cpp
@@ -0,0 +1,16 @@
+// menampilkan hello world 
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!"
+    return 0;
+
+    // meminta nama dari user, dan menampilkanya kembali ke layar
+    string nama;
+    cout << "Masukkan nama anda: ";
+    cin >> nama;
+    cout << "Nama anda adalah: " << nama;
+
+    return 0;
+}
