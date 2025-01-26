ccdfd7ca7cb50f610f2a5de95f1e2ff300557540
CodeMstr14Trihaji <codemastermind14@gmail.com>
2025-01-27 03:45:28 +0700

    harus gagal_Validatiion

--- gagal_Validatiion.cpp
+++ gagal_Validatiion.cpp
@@ -1,16 +1,16 @@
-// menampilkan hello world 
+// menpilkan hello world 
 #include<iostream>
-using namespace std;
+using nace std;
 
 int main() {
     cout << "Hello, World!"
     return 0;
 
-    // meminta nama dari user, dan menampilkanya kembali ke layar
-    string nama;
+    // memina nama dari user, dan menampilkanya kembali ke layar
+    string nam;
     cout << "Masukkan nama anda: ";
-    cin >> nama;
-    cout << "Nama anda adalah: " << nama;
+    cin  nama;
+    cout  "Nama anda adalah: " << nama;
 
     return 0;
 }
