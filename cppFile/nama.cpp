18fde41d83ebd267c0e121491791b48af8a14e29
Trihaji Khoerur Rohman <codemastermind14@gmail.com>
2025-01-27 15:52:15 +0700

    Create nama.cpp

---
+++ cppFile/nama.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namepace std;
+
+int main(){
+  string nama;
+  cout << "Masukan nama anda: ";
+  cin >> nama;
+
+  cout << "Nama anda adalah: " << nama << endl;
+
+return 0;
+}
